a9a997af628124c4f9a61d44dfc74beaa0f55d8e
SheogorathD <aknojlogiya@gmail.com>
2022-05-21 23:27:13 +0300

    MORE ERRORS

--- api/main.js
+++ api/main.js
@@ -24,8 +24,6 @@ if (process.env.NODE_ENV === "production") {
       path.resolve(__dirname, "../", "frontend", "build", "index.html")
     )
   );
-} else {
-  app.get("/", (req, res) => res.send("Set to production mode"));
 }
 
 app.use(errorHandler);
